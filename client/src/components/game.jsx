6f2594462cfee50cbb519be01a48282e92dce245
Josh Alamillo <josh.alamillo@gmail.com>
2018-06-02 15:14:42 -0700

    Basic html page rendered

---
+++ client/src/components/game.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+
+class Game extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>Block Barrage is on its way!</div>
+      );
+  }
+
+}
+
+export default Game
---
+++ client/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import reactDOM from 'react-dom'
+import Game from './components/game.jsx'
+
+reactDOM.render(<Game />, document.getElementById('game'))
