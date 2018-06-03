4b6395950441960820a1ac93598f7547a73de55a
Josh Alamillo <josh.alamillo@gmail.com>
2018-06-02 20:34:58 -0700

    Wrote tests for initializeGrid

--- client/src/components/column.jsx
+++ client/src/components/column.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Column = ( {grid, row, allColumns} ) => {
 
   return allColumns.map((column) => {
-    return <span id={`${row},${column}`} className='square' background-color={grid[[row, column]]}></span>
+    return <span id={`${row},${column}`} className='square' background-color={grid[[row, column]]}>{row},{column}</span>
   })
 }
 
