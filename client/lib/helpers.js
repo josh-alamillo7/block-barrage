0bfcc102fa9661331f6ebac82f16872dbf4ae1ca
Josh Alamillo <josh.alamillo@gmail.com>
2018-06-04 10:40:23 -0700

    wore first test for genNewBlock

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -14,22 +14,61 @@ const initializeGrid = (height, width) => {
   return output
 }
 
+const placeBlockOnGrid = (grid, column, newBlock, colorOne, colorTwo) => {
+  console.log('this was called')
+  newBlock['colorOne'] = colorOne;
+  newBlock['colorTwo'] = colorTwo;
+  grid[[0,column]] = colorOne;
+  grid[[1,column]] = colorOne;
+  grid[[2,column]] = colorTwo;
+  grid[[3,column]] = colorTwo;
+}
+
 const genNewBlock = (grid, width) => {
   //choose a random column and check its color
   let checked = {};
   let newBlock = {};
-  let randomNumber
+  let possibleColors = ['red', 'white', 'black', 'green', 'yellow', 'blue', 'orange']
+  let randomColumn;
 
   while (Object.keys(checked).length < width) {
-    randomNumber = Math.floor(Math.random() * width)
-    console.log(randomNumber)
-    checked[randomNumber] = true
+    randomColumn = Math.floor(Math.random() * width)
+    console.log(randomColumn)
+    if (grid[[3,randomColumn]] === 'grey') {
+      newBlock['column'] = randomColumn
+      newBlock['secBlockPosition'] = 2
+      break
+    }
+    checked[randomColumn] = true
   }
 
-  return grid
-}
+  if (newBlock['column'] === undefined) {
+    return {
+      grid: grid,
+      newBlock: null,
+      action: 'gameOver'
+    }
+  }
 
-genNewBlock({}, 4)
+  const colorOne = possibleColors[Math.floor(Math.random() * 7)]
+  let colorTwo = undefined
+  let potentialColor
+
+  while (colorTwo === undefined) {
+    potentialColor = possibleColors[Math.floor(Math.random() * 7)]
+    if (potentialColor !== colorOne) {
+      colorTwo = potentialColor
+    }
+  }
+
+  placeBlockOnGrid(grid, randomColumn, newBlock, colorOne, colorTwo)
+
+  return {
+    grid: grid,
+    newBlock: newBlock,
+    action: 'drop'
+  }
+}
 
 module.exports.return = returnMe;
 module.exports.initializeGrid = initializeGrid;
--- client/lib/helpers.test.js
+++ client/lib/helpers.test.js
@@ -8,3 +8,26 @@ test('initializeGrid should return an object with the correct number of keys', (
   expect(helpers.initializeGrid(3, 4)[[2, 3]]).toBe('grey')
   expect(Object.keys(helpers.initializeGrid(5, 8)).length).toBe(40)
 })
+
+test('genNewBlock should return a new grid with the two-colored block on a random available column', () => {
+  const testGridOne = helpers.initializeGrid(8, 4);
+  let resultGrid = helpers.genNewBlock(testGridOne, 4).grid
+  let colors = []
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 3; j++) {
+      colors.push(resultGrid[[i,j]])
+    }
+  }
+  let nonGrays = colors.filter((color) => {
+    return color !== 'grey'
+  })
+  expect(nonGrays.length).toBe(4)
+})
+
+test('genNewBlock should only choose an available column if there are unavailable columns', () => {
+
+})
+
+test('genNewBlock should return the same grid, no block, and a game over state if there are no free columns', () => {
+
+})
