40f16d42dfbea6e070fd95832412aa770068fb97
Josh Alamillo <josh.alamillo@gmail.com>
2018-06-11 15:45:04 -0700

    Added server

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const server = http.createServer((request, response) => {
+  console.log('serving')
+
+  let filePath = '/Users/joshalamillo/Desktop/Personal Projects/minigames/block-barrage/client/dist' + request.url
+
+
+  if (request.url == '/') {
+    filePath += 'index.html';
+  }
+
+  // const filePath = '/Users/joshalamillo/Desktop/Personal Projects/minigames/block-barrage/client/dist/index.html'
+
+  const extname = String(path.extname(filePath)).toLowerCase();
+  
+  const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css'
+  }
+
+  const contentType = mimeTypes[extname]
+
+  const fileToServe = fs.readFileSync(filePath)
+
+  response.end(fileToServe)
+
+})
+
+server.listen(3330, function() {
+  console.log('listening on port 3330...')
+})
