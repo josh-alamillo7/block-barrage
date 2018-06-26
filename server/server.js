a171af1fcd9b3e3812338a4a776fdd7942012244
Josh Alamillo <josh.alamillo@gmail.com>
2018-06-26 11:23:15 -0700

    Removed console on server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const fs = require('fs')
 const path = require('path')
 
 const server = http.createServer((request, response) => {
-  console.log('serving')
 
   let filePath = '/Users/joshalamillo/Desktop/Personal Projects/minigames/block-barrage/client/dist' + request.url
 
